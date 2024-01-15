ab48dc05b684691cde2de22d90dab73fe075a5d3
michkaiser <michkaiser13@gmail.com>
2024-01-15 22:28:37 +0300

    first commit

---
+++ Appetizers/AccountView.swift
@@ -0,0 +1,23 @@
+//
+//  AccountView.swift
+//  Appetizers
+//
+//  Created by Kaiser on 15/01/2024.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        NavigationView{
+            Text("Account")
+                .navigationTitle("Account")
+        }
+    }
+}
+
+struct AccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountView()
+    }
+}
